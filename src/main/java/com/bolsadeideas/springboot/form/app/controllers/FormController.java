404b967239b8149cdb4b81ead2ef2c041f25d384
jcmartinez <jcmartinezcu@gmail.com>
2020-06-15 14:44:40 +0200

    video 61

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -4,6 +4,7 @@ package com.bolsadeideas.springboot.form.app.controllers;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -13,10 +14,14 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
+import com.bolsadeideas.springboot.form.app.validation.UsuarioValidador;
 
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
+	
+	@Autowired
+	private UsuarioValidador validador;
 	
 	@GetMapping("/form")
 	public String form(Model model) {
@@ -33,6 +38,7 @@ public class FormController {
 	
 	@PostMapping("/form")
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
+		validador.validate(usuario, result);
 		
 		model.addAttribute("titulo", "Resultado form");
 		
--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -2,15 +2,15 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+//import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 public class Usuario {
 	
-	@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
+//	@Pattern(regexp = "[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")
 	private String identificador;
 
-	@NotEmpty(message = "El nombre no puede estar vacio!!")
+	//@NotEmpty(message = "El nombre no puede estar vacio!!")
 	private String nombre;
 	
 	@NotEmpty
