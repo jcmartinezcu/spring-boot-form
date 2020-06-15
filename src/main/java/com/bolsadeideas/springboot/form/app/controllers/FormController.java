f11a3087a1bbcfc255aa917019769dfee5a410ce
jcmartinez <jcmartinezcu@gmail.com>
2020-06-15 11:55:56 +0200

    video 56

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -9,10 +9,13 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
 
 import com.bolsadeideas.springboot.form.app.models.domain.Usuario;
 
 @Controller
+@SessionAttributes("usuario")
 public class FormController {
 	
 	@GetMapping("/form")
@@ -29,7 +32,7 @@ public class FormController {
 	}
 	
 	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 		
 		model.addAttribute("titulo", "Resultado form");
 		
@@ -39,7 +42,7 @@ public class FormController {
 		}
 		
 		model.addAttribute("usuario", usuario);
-		
+		status.setComplete();
 		return "resultado";
 	}
 
