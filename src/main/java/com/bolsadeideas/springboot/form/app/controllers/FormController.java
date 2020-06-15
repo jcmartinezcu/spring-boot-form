e4080a6e3149276ecd48abbc0211ac6984e6e35b
jcmartinez <jcmartinezcu@gmail.com>
2020-06-15 16:15:09 +0200

    video 62

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
@@ -22,6 +24,11 @@ public class FormController {
 	
 	@Autowired
 	private UsuarioValidador validador;
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(validador);
+	}
 	
 	@GetMapping("/form")
 	public String form(Model model) {
@@ -38,7 +45,7 @@ public class FormController {
 	
 	@PostMapping("/form")
 	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
-		validador.validate(usuario, result);
+		//validador.validate(usuario, result);
 		
 		model.addAttribute("titulo", "Resultado form");
 		
--- src/main/java/com/bolsadeideas/springboot/form/app/validation/UsuarioValidador.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/validation/UsuarioValidador.java
@@ -20,7 +20,7 @@ public class UsuarioValidador implements Validator {
 
 		Usuario usuario = (Usuario)target;
 		
-		ValidationUtils.rejectIfEmpty(errors, "nombre", "NotEmpty.usuario.nombre");
+		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nombre", "requerido.usuario.nombre");
 		
 		if(!usuario.getIdentificador().matches("[0-9]{2}[.][\\d]{3}[.][\\d]{3}[-][A-Z]{1}")) {
 			errors.rejectValue("identificador", "pattern.usuario.identificador");
