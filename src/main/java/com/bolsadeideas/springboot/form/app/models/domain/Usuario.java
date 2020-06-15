d7b7fb44207708ec02bc8d1a6f4328c143d10274
jcmartinez <jcmartinezcu@gmail.com>
2020-06-15 12:02:03 +0200

    video 57

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,24 +1,28 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 public class Usuario {
 	
 	private String identificador;
 
-	@NotEmpty
+	@NotEmpty(message = "El nombre no puede estar vacio!!")
 	private String nombre;
 	
 	@NotEmpty
 	private String apellido;
 
 	@NotEmpty
+	@Size(min = 3, max = 8)
 	private String username;
 
 	@NotEmpty
 	private String password;
 
 	@NotEmpty
+	@Email(message = "La direccion de correo no es valida!!")
 	private String email;
 
 	public String getUsername() {
