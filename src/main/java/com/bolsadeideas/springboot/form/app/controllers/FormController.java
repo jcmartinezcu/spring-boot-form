e7f97b1b2778b066f9a9e6e4149dba9d82560e03
jcmartinez <jcmartinezcu@gmail.com>
2020-06-15 11:37:43 +0200

    video 54

--- src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/controllers/FormController.java
@@ -1,7 +1,6 @@
 package com.bolsadeideas.springboot.form.app.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 import javax.validation.Valid;
 
@@ -32,11 +31,7 @@ public class FormController {
 		model.addAttribute("titulo", "Resultado form");
 		
 		if(result.hasErrors()) {
-			Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
+			
 			return "form";
 		}
 		
