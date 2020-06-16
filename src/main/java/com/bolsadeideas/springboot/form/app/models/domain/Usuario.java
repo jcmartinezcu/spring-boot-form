4ecf147eecb85c7b5d4c7f79d788054b34dc25bb
jcmartinez <jcmartinezcu@gmail.com>
2020-06-16 14:35:56 +0200

    video 74

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -2,6 +2,7 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 
 import java.util.Date;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 //import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
@@ -52,8 +53,8 @@ public class Usuario {
 	@Past
 	private Date fechaNacimiento;
 
-	@NotEmpty
-	private String pais;
+	@Valid
+	private Pais pais;
 
 	public String getUsername() {
 		return username;
@@ -119,11 +120,11 @@ public class Usuario {
 		this.fechaNacimiento = fechaNacimiento;
 	}
 
-	public String getPais() {
+	public Pais getPais() {
 		return pais;
 	}
 
-	public void setPais(String pais) {
+	public void setPais(Pais pais) {
 		this.pais = pais;
 	}
 
