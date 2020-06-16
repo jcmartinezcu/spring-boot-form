b17564cb147575873046ddb46c5313e1d5fa09f0
jcmartinez <jcmartinezcu@gmail.com>
2020-06-16 12:09:45 +0200

    video 68

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -9,11 +9,11 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
+//import javax.validation.constraints.Past;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
-import org.springframework.format.annotation.DateTimeFormat;
+//import org.springframework.format.annotation.DateTimeFormat;
 
 import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
 import com.bolsadeideas.springboot.form.app.validation.Requerido;
@@ -48,7 +48,7 @@ public class Usuario {
 	private Integer cuenta;
 
 	@NotNull
-	@DateTimeFormat(pattern  = "yyyy-MM-dd")
+	//@DateTimeFormat(pattern  = "yyyy-MM-dd")
 	@Future
 	private Date fechaNacimiento;
 
