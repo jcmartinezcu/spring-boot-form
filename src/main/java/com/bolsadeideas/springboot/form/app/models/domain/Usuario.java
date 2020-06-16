8384d4e5882f87d08427a467d26711440ee148be
jcmartinez <jcmartinezcu@gmail.com>
2020-06-16 11:50:58 +0200

    video 66

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.form.app.models.domain;
 
+import java.util.Date;
+
 import javax.validation.constraints.Email;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
@@ -8,6 +10,8 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+
+import org.springframework.format.annotation.DateTimeFormat;
 
 import com.bolsadeideas.springboot.form.app.validation.IdentificadorRegex;
 import com.bolsadeideas.springboot.form.app.validation.Requerido;
@@ -40,6 +44,10 @@ public class Usuario {
 	@Min(5)
 	@Max(5000)
 	private Integer cuenta;
+
+	@NotNull
+	@DateTimeFormat(pattern  = "yyyy-MM-dd")
+	private Date fechaNacimiento;
 
 	public String getUsername() {
 		return username;
@@ -95,6 +103,14 @@ public class Usuario {
 
 	public void setCuenta(Integer cuenta) {
 		this.cuenta = cuenta;
+	}
+
+	public Date getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(Date fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
 	}
 
 }
