ea68fdc4565934f6a99f07f456a8ab1543ff2fea
jcmartinez <jcmartinezcu@gmail.com>
2020-06-16 12:00:01 +0200

    video 67

--- src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/models/domain/Usuario.java
@@ -3,11 +3,13 @@ package com.bolsadeideas.springboot.form.app.models.domain;
 import java.util.Date;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.Future;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
 //import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
@@ -47,6 +49,7 @@ public class Usuario {
 
 	@NotNull
 	@DateTimeFormat(pattern  = "yyyy-MM-dd")
+	@Future
 	private Date fechaNacimiento;
 
 	public String getUsername() {
