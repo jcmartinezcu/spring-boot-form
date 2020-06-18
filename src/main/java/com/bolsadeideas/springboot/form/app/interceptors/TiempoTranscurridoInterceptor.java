85e7e014b3693d068716798ed66183a3679973b5
jcmartinez <jcmartinezcu@gmail.com>
2020-06-18 12:44:18 +0200

    video 91

--- src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -1,25 +1,50 @@
 package com.bolsadeideas.springboot.form.app.interceptors;
 
+import java.util.Random;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
+	
+	private static final Logger logger = LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class);
 
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
+		logger.info("TiempoTranscurridoInterceptor: preHandle() entrando ...");
+		long tiempoInicio = System.currentTimeMillis();
+		request.setAttribute("tiempoInicio", tiempoInicio);
+		
+		Random randon = new Random();
+		Integer demora = randon.nextInt(500);
+		Thread.sleep(demora);
+		
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
+		
+		long tiempoFin = System.currentTimeMillis();
+		long tiempoInicio = (Long)request.getAttribute("tiempoInicio");
+		long tiempoTrasncurrido = tiempoFin - tiempoInicio;
+		
+		if(modelAndView != null) {
+			modelAndView.addObject("tiempoTrasncurrido", tiempoTrasncurrido);
+		}
+		
+		logger.info("Tiempo transcurrido: " + tiempoTrasncurrido + " milisegundos");
+		logger.info("TiempoTranscurridoInterceptor: postHandle() saliendo ...");
 	}
 
 }
