9805d464346c5b60b5cb33f1434315a747d1a363
jcmartinez <jcmartinezcu@gmail.com>
2020-06-18 16:56:12 +0200

    video 94

--- src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/bolsadeideas/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -46,6 +46,10 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
 		
+		if(request.getMethod().equalsIgnoreCase("post")) {
+			return;
+		}
+		
 		long tiempoFin = System.currentTimeMillis();
 		long tiempoInicio = (Long)request.getAttribute("tiempoInicio");
 		long tiempoTrasncurrido = tiempoFin - tiempoInicio;
